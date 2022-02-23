193c1b307140dbf87b96f5ca0cd604860076dd52
Sadie Allen <sadiela@bu.edu>
2022-02-23 11:22:39 -0500

    try jsonschema on mac

--- device_module/device_noflask.py
+++ device_module/device_noflask.py
@@ -3,6 +3,30 @@ sys.path.append('..')
 from device_module.helper import *
 #from helper import *
 import json
+from jsonschema import validate
+
+patient_data = {  
+   'key': 'abc',
+   'name': 'John Doe',
+   'data': [
+            {  'data_type': 'blood_pressure',
+               'values': [['2020-03-27T19:46:21', '120/80']]
+            },
+            {  'data_type': 'temperature',
+               'values': [['2020-03-27T19:46:21', 98.6],['2020-03-28T19:46:21', 99.6]]
+            }
+           ]
+    }
+
+patient_data_schema = {
+  "type":"object",
+  "properties": {
+    "key":{"type":"string"},
+    "name":{"type":"string"},
+    "data":{"type":"list"}
+  }
+
+}
 
 
 def is_json(myjson):
@@ -29,3 +53,9 @@ def add_patient_data(json_obj):
         return {'error':'Request must be JSON'}, 415
 
 print(is_json('NOT:A:JSON'))
+
+try:
+    validate(instance=patient_data, schema=[patient_data_schema])
+except jsonschema.exceptions.ValidationError as err:
+    print("EXCEPTION")
+print("VALID")
